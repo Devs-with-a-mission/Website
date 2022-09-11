c2ab5d540ade3b6dd8123d8a51e5f638d245e1e4
Samuel Huss <82036180+samuelhuss@users.noreply.github.com>
2022-09-11 14:21:41 -0300

    add new resources

--- src/screens/register/register.js
+++ src/screens/register/register.js
@@ -1,10 +1,31 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 import Navbar from "../../components/header/header";
 import Footer from "../../components/footer/footer";
 import banner from "../../components/img/banner.png"
-import {Link} from 'react-router-dom'
+import {Link, Navigate} from 'react-router-dom'
+import UsersService from "../../services/users";
 
 const Register = () => {
+
+     const [name, setName] = useState("");
+     const [email, setEmail] = useState("");
+     const [password, setPassword] = useState("");
+     const [redirectToLogin, setRedirectToLogin] = useState(false);
+     const [error, setError] = useState(false);
+
+    const HandleSubmit = async(evt) => {
+        evt.preventDefault()
+        try {
+            const user = await UsersService.register({name: name, email: email, password: password})
+            setRedirectToLogin(true)
+        } catch (error) {
+            setError(true)
+        }
+    }
+
+    if(redirectToLogin)
+    return <Navigate to={{pathname: "/login"}}/>
+
     return(
     <Fragment>
         <Navbar/>
@@ -13,10 +34,12 @@ const Register = () => {
       <div className="hidden bg-cover lg:block lg:w-1/2">
         <img id="form_IMG" className="flex items-center ml-12 mt-12" src={banner}/>
       </div>
-  
-      <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
+    
+      <div  className="w-full px-6 py-8 md:px-8 lg:w-1/2">
         
-  
+         <form onSubmit={HandleSubmit} >
+
+        
           <p className="text-xl text-center text-gray-700 ">
               Register now
           </p>
@@ -27,12 +50,24 @@ const Register = () => {
 
           <div className="mt-4">
               <label className="block mb-2 text-sm font-medium text-gray-600 " for="loggingName">Name</label>
-              <input id="LoggingEmailAddress" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md  focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300" type="email" />
+              <input 
+                 type="name"
+                 required
+                 name="name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+               id="LoggingEmailAddress" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md  focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300" />
           </div>
   
           <div className="mt-4">
               <label className="block mb-2 text-sm font-medium text-gray-600 " for="LoggingEmailAddress">Email Address</label>
-              <input id="LoggingEmailAddress" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md  focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300" type="email" />
+              <input
+                 type="email"
+                required
+                name="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+               id="LoggingEmailAddress" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md  focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"  />
           </div>
   
           <div className="mt-4">
@@ -40,7 +75,13 @@ const Register = () => {
                   <label className="block mb-2 text-sm font-medium text-gray-600 " for="loggingPassword">Password</label>
               </div>
   
-              <input id="loggingPassword" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" />
+              <input
+               type="password"
+               required
+               name="password"
+               value={password}
+               onChange={e => setPassword(e.target.value)}
+              id="loggingPassword" className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md focus:border-blue-400 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" />
           </div>
   
           <div className="mt-8">
@@ -52,10 +93,11 @@ const Register = () => {
           <div className="flex items-center justify-between mt-4">
               <span className="w-1/5 border-b md:w-1/4"></span>
   
-              <Link to='/login' className="text-xs text-gray-500 uppercase  hover:underline">or sign in</Link>
+              <a onClick={e => setRedirectToLogin(true)} className="text-xs text-gray-500 uppercase  hover:underline">or sign in</a>
   
               <span className="w-1/5 border-b md:w-1/4"></span>
           </div>
+          </form>
       </div>
   </div>
         </div>
