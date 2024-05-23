1756760f206a99348a09695e34777d94bd1e7317
Pedro Hedro <70533287+pedrohedro@users.noreply.github.com>
2024-05-23 22:27:16 +0000

    remove ecs

--- Infra/terraform/main.tf
+++ Infra/terraform/main.tf
@@ -3,18 +3,3 @@ module "new-vpc" {
   prefix         = var.prefix
   vpc_cidr_block = var.vpc_cidr_block
 }
-module "ecs" {
-  source          = "./modules/Ecs"
-  cluster_name    = "${var.prefix}-ecs"
-  service_name    = "${var.prefix}-service"
-  task_definition = "${var.prefix}-task"
-  vpc_id          = module.new-vpc.vpc_id
-  subnets         = module.new-vpc.subnet_ids
-  container_port  = 80
-  desired_count   = 2
-  region          = "us-east-1"
-
-  depends_on = [
-    module.new-vpc
-  ]
-}
